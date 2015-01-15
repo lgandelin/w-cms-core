493fb8b5ec7c8d9916ac164a1bd63cb3fcd20e79
Louis Gandelin <lgandelin@web-access.fr>
2015-01-15 10:42:29 +0100

    Extracted GetPageInfoFromMasterInteractor class

---
+++ src/CMS/Interactors/Pages/GetPageInfoFromMasterInteractor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CMS\Interactors\Pages;
+
+class GetPageInfoFromMasterInteractor
+{
+    private $getPageInteractor;
+
+    public function __construct(GetPageInteractor $getPageInteractor)
+    {
+        $this->getPageInteractor = $getPageInteractor;
+    }
+
+    public function getPageStructure($masterPageID, $pageTitle)
+    {
+        $pageStructure = \App::make('GetPageInteractor')->getPageByID($masterPageID, true);
+        $pageStructure->name = $pageTitle;
+        $slug = strtolower(str_replace(' ', '-', $pageTitle));
+        $pageStructure->uri = '/' . $slug;
+        $pageStructure->identifier = $slug;
+        $pageStructure->is_master = 0;
+        $pageStructure->master_page_id = $masterPageID;
+
+        return $pageStructure;
+    }
+} 
