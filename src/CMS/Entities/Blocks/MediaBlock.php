5da3a025f836b45da81d5d1cdc3716429a1dd09d
Louis Gandelin <lgandelin@web-access.fr>
2015-02-04 12:03:28 +0100

    Updated MediaBlock and MediaBlockStructure classes after refactoring

--- src/CMS/Entities/Blocks/MediaBlock.php
+++ src/CMS/Entities/Blocks/MediaBlock.php
@@ -3,6 +3,8 @@
 namespace CMS\Entities\Blocks;
 
 use CMS\Entities\Block;
+use CMS\Structures\Blocks\MediaBlockStructure;
+use CMS\Structures\BlockStructure;
 
 class MediaBlock extends Block
 {
@@ -38,5 +40,30 @@ class MediaBlock extends Block
     public function getMediaFormatID()
     {
         return $this->mediaFormatID;
+    }
+
+    public function getStructure()
+    {
+        $blockStructure = new MediaBlockStructure();
+        $blockStructure->media_id = $this->getMediaID();
+        $blockStructure->media_link = $this->getMediaLink();
+        $blockStructure->media_format_id = $this->getMediaFormatID();
+
+        return $blockStructure;
+    }
+
+    public function updateContent(BlockStructure $blockStructure)
+    {
+        if ($blockStructure->media_id !== null && $blockStructure->media_id != $this->getMediaID()) {
+            $this->setMediaID($blockStructure->media_id);
+        }
+
+        if ($blockStructure->media_link !== null && $blockStructure->media_link != $this->getMediaLink()) {
+            $this->setMediaLink($blockStructure->media_link);
+        }
+
+        if ($blockStructure->media_format_id !== null && $blockStructure->media_format_id != $this->getMediaFormatID()) {
+            $this->setMediaFormatID($blockStructure->media_format_id);
+        }
     }
 }
--- src/CMS/Structures/Blocks/MediaBlockStructure.php
+++ src/CMS/Structures/Blocks/MediaBlockStructure.php
@@ -2,6 +2,7 @@
 
 namespace CMS\Structures\Blocks;
 
+use CMS\Entities\Blocks\MediaBlock;
 use CMS\Structures\BlockStructure;
 
 class MediaBlockStructure extends BlockStructure
@@ -9,4 +10,9 @@ class MediaBlockStructure extends BlockStructure
     public $media_id;
     public $media_link;
     public $media_format_id;
+
+    public function getBlock()
+    {
+        return new MediaBlock();
+    }
 } 
