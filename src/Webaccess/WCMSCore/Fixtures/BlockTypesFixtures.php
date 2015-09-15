231ed4f422444d9d125c88a3a5889bf5b34fa6ef
Louis Gandelin <lgandelin@web-access.fr>
2015-09-15 14:43:22 +0200

    Renamed BlockController classes to BlockFormsController

--- src/Webaccess/WCMSCore/Fixtures/BlockTypesFixtures.php
+++ src/Webaccess/WCMSCore/Fixtures/BlockTypesFixtures.php
@@ -11,10 +11,10 @@ class BlockTypesFixtures {
     {
         $blockTypes = [
             ['code' => 'html', 'name' => 'Block HTML', 'back_controller' => null, 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.html', 'front_view' => 'blocks.standard.html', 'order' => 1],
-            ['code' => 'menu', 'name' => 'Block Menu', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\MenuBlockController', 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.menu', 'front_view' => 'blocks.standard.menu', 'order' => 2],
-            ['code' => 'article', 'name' => 'Block Article', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\ArticleBlockController', 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.article', 'front_view' => 'blocks.standard.article', 'order' => 3],
-            ['code' => 'article_list', 'name' => 'Block Article list', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\ArticleListBlockController', 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.article_list', 'front_view' => 'blocks.standard.article_list', 'order' => 4],
-            ['code' => 'media', 'name' => 'Block Media', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\MediaBlockController', 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.media', 'front_view' => 'blocks.standard.media', 'order' => 5],
+            ['code' => 'menu', 'name' => 'Block Menu', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\MenuBlockFormController', 'back_view' => null, 'front_view' => 'blocks.standard.menu', 'order' => 2],
+            ['code' => 'article', 'name' => 'Block Article', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\ArticleBlockFormController', 'back_view' => null, 'front_view' => 'blocks.standard.article', 'order' => 3],
+            ['code' => 'article_list', 'name' => 'Block Article list', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\ArticleListBlockFormController', 'back_view' => null, 'front_view' => 'blocks.standard.article_list', 'order' => 4],
+            ['code' => 'media', 'name' => 'Block Media', 'back_controller' => 'Webaccess\WCMSLaravel\Http\Controllers\Back\Editorial\Blocks\MediaBlockFormController', 'back_view' => null, 'front_view' => 'blocks.standard.media', 'order' => 5],
             ['code' => 'view', 'name' => 'Block View', 'back_controller' => null, 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.view', 'front_view' => 'blocks.standard.view', 'order' => 6],
             ['code' => 'controller', 'name' => 'Block Controller', 'back_controller' => null, 'back_view' => 'w-cms-laravel::back.editorial.pages.blocks.controller', 'front_view' => 'blocks.standard.controller', 'order' => 7],
         ];
