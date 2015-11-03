b037844f8545f53e3ed3a826690958f058816249
Louis Gandelin <lgandelin@web-access.fr>
2015-11-03 17:13:53 +0100

    Added DeleteMediaFolderInteractor

---
+++ tests/Webaccess/WCMSCoreTests/Interactors/MediaFolders/DeleteMediaFolderInteractorTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Webaccess\WCMSCoreTests\Interactors\MediaFolders;
+
+use Webaccess\WCMSCore\Context;
+use Webaccess\WCMSCore\Entities\MediaFolder;
+use Webaccess\WCMSCore\Interactors\MediaFolders\DeleteMediaFolderInteractor;
+use CMSTestsSuite;
+
+class DeleteMediaFolderInteractorTest extends \PHPUnit_Framework_TestCase {
+
+    private $interactor;
+
+    public function setUp() {
+        CMSTestsSuite::clean();
+        $this->interactor = new DeleteMediaFolderInteractor();
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testDeleteNonExistingMediaFolder()
+    {
+        $this->interactor->run(1);
+    }
+
+    public function testDeleteMediaFolder()
+    {
+        $this->createSampleMediaFolder();
+
+        $this->assertCount(1, Context::get('media_folder_repository')->findAll());
+
+        $this->interactor->run(1);
+
+        $this->assertCount(0, Context::get('media_folder_repository')->findAll());
+    }
+
+    private function createSampleMediaFolder()
+    {
+        $mediaFolder = new MediaFolder();
+        $mediaFolder->setID(1);
+        $mediaFolder->setName('Test media folder');
+        $mediaFolder->setPath('/test-media-folder');
+        Context::get('media_folder_repository')->createMediaFolder($mediaFolder);
+    }
+}
